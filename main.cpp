f30332dc400fd3575c3cf127d37bc9a492d8d621
shaqsnake <shaqsnake@gmail.com>
2019-06-05 14:57:22 +0800

    Support set demo version and show version info.

--- main.cpp
+++ main.cpp
@@ -2,25 +2,27 @@
 #include <stdlib.h>
 #include "config.h"
 
-#ifdef USE_MYMATH
-#include "math/MathFunc.hpp"
-#else
+#ifdef HAVE_SQRT
 #include <math.h>
+#else
+#include <MathFunc.hpp>
 #endif
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
+        // Show version info
+        printf("%s Verion %d.%d\n", argv[0], Demo_VERSION_MAJOR, Demo_VERSION_MINOR);
         fprintf(stdout, "Usage: %s number\n", argv[0]);
         return 1;
     }
 
     double inputValue = atof(argv[1]);
-#ifdef USE_MYMATH
-    printf("Using my math lib...\n");
-    double outputValue = squareRoot(inputValue);
-#else
+#ifdef HAVE_SQRT
     printf("Using the std lib...\n");
     double outputValue = sqrt(inputValue);
+#else
+    printf("Using my math lib...\n");
+    double outputValue = squareRoot(inputValue);
 #endif
     fprintf(stdout, "The square root of %g is %g\n", inputValue, outputValue);
     return 0;
